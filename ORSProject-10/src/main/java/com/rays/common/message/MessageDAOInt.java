e429ba702fdd31c60488750de495807e6a27eb11
Your Name <kapildudhe0@gmail.com>
2024-04-06 22:49:52 +0530

    done

---
+++ ORSProject-10/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Kapil Dudhe
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
